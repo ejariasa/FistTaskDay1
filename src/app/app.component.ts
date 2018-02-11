45a4475d09473270c798ad9155eb1996c83d499c
ejariasa <yuxi-eylen@hotmail.com>
2018-02-11 10:57:16 -0500

    task 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'app';
+	title = 'my application';
+	author = 'Eylen Arias';
+	count = 0;
+	imgSource = './../favicon.ico';
+	enabled = true;
+
+	ChangeState() {
+		this.enabled = !this.enabled;
+	}
 }
